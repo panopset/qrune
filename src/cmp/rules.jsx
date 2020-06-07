ef899febd04f6f48cdeef9bb4801e1ca43e173a9
Karl Dinwiddie <karldinwiddie@gmail.com>
2020-06-07 07:39:29 -0700

    Introduce some organization with imports.

---
+++ src/cmp/rules.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { getElement } from './runes.jsx';
+
+export default class Board extends React.Component {
+ render() {
+  return getElement();
+ }
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './cmp/rules.jsx';
 
 var x = 0;
+var y = 0;
 
-const element = <div>
-
-
-
-<div>
-<button class="gobtn">Go</button>
-<a href="https://github.com/panopset/qrune">Source.</a>
-</div>
-
-<div class="qr">
-.
-</div>
-
-<svg height="140" width="500">
-  <ellipse class="yb" cx="200" cy="80" rx="100" ry="50" />
-</svg>
-
-<svg id="foo" height="140" width="500">
-  <ellipse class="bb" cx="200" cy="80" rx="100" ry="50" />
-</svg>
-
-</div>;
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-header">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
- element,
+ <Game />,
  document.getElementById('root')
 );
-
-var yb = document.querySelectorAll(".yb");
 
 var theBoxes = document.querySelectorAll(".qr");
 
@@ -42,13 +31,6 @@ require("kute.js/kute-attr");
 require("kute.js/kute-text");
 
 var animateFontSize = kute.allFromTo(
-  theBoxes,
-  { fontSize: '2em' },
-  { fontSize: '4em' },
-  { offset: 100, duration: 200, repeat: 10, yoyo: true}
-);
-
-var animateLocation = kute.allFromTo(
   theBoxes,
   { fontSize: '2em' },
   { fontSize: '4em' },
@@ -68,11 +50,14 @@ goButton.addEventListener(
 var anim = document.getElementById('foo');
 
 function step() {
- if (x > 500) {
+ if (y++ > 20) {
+  y = 0;
+ }
+ if (x++ > 50) {
   x = 0;
  }
  x++;
- anim.innerHTML='<ellipse class="bb" cx="' + (200 + x) + '" cy="80" rx="100" ry="50" />';
+ anim.innerHTML='<ellipse class="bb" cx="' + (200 + x) + '" cy="' + (80 + y) + '" rx="100" ry="50" />';
  window.requestAnimationFrame(step);
 }
 
