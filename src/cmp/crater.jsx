fddc3ea745c62e554caa97150bddcc5ec757c530
Karl Dinwiddie <karldinwiddie@gmail.com>
2020-06-13 23:32:29 -0700

    .

--- src/cmp/crater.jsx
+++ src/cmp/crater.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { move } from './move.js';
 
 export default class Crater extends Component {
- 
+
  constructor(props) {
   super(props);
   this.dorpRef = React.createRef();
@@ -10,64 +10,22 @@ export default class Crater extends Component {
 
  render() {
   return (
-  
-  <div className="bb" >
-
-<svg height="80" width="500">
-<g>
-  <ellipse className="ab" cx="200" cy="20" rx="100" ry="50" />
-</g>
-</svg>
-
-<svg ref={this.dorpRef} height="100" width="500"/>
-
-</div>
-
-
+<div className="bb" ><svg ref={this.dorpRef} height="100" width="500"/></div>
   );
  }
- 
- 
+
  componentDidMount() {
-    
-var x = 0;
-var y = 0;
-var coords = [{x: 0, y: 0}];
-    
-
-var anim = this.dorpRef.current;
-
- function step() {
-    if (y++ > 20) {
-    y = 0;
-   }
-   if (x++ > 50) {
-    x = 0;
-   }
-   x++;
-   
-   
-   anim.innerHTML='<ellipse cx="200" cy="20" rx="100" ry="30" />';
-   
+  var coords = {x: 0, y: 0};
+  var anim = this.dorpRef.current;
+  function step() {
+   foo();
    window.requestAnimationFrame(step);
   }
   window.requestAnimationFrame(step);
-  foo();
-  
   function foo() {
-  
-  
-   anim.innerHTML='<ellipse cx="' + (200 + x) + '" cy="' + (20 + y) + '" rx="100" ry="30" />';
-  
-  
-  var rslt = move(coords);
-   
-   coords = rslt.coords;
-
-  
+   var rslt = move(coords);
+   coords = rslt.cds;
+   anim.innerHTML = rslt.innerHTML;
   }
-  
-  
  }
- 
 }
--- src/cmp/move.js
+++ src/cmp/move.js
@@ -1,18 +1,15 @@
 export function move(coords) {
-
-    if (coords.y++ > 20) {
-    coords.y = 0;
-   }
-   if (coords.x++ > 50) {
-    coords.x = 0;
-   }
-   coords.x++;
-   
-   var ih = '<ellipse cx=' + (200 + coords.x) + ' cy="' + (20 + coords.y) + '" rx="100" ry="30" />'
-   
-   
- return {
-  innerHtml: ih,
-  coords: coords,
- };
+ if (coords.y++ > 20) {
+  coords.y = 0;
+  }
+  if (coords.x++ > 50) {
+   coords.x = 0;
+  }
+  coords.x++;
+  var ih = '<ellipse cx=' + (200 + coords.x) + ' cy="' + (20 + coords.y) + '" rx="100" ry="30" />'
+  var rtn = {
+   innerHTML: ih,
+   cds: coords,
+  };
+ return rtn;
 }
